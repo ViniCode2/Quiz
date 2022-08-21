8dab2e3ccacf1dd3efb119e1b9abb70ed45f813a
Vinicius Gomes <85400320+ViniCode2@users.noreply.github.com>
2022-08-21 17:18:48 -0300

    Quiz
    Quiz do filme - O jogo da imitação

---
+++ quiz-master/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Em qual capital ficava a cede da equipe de decodificadores?",
+    answer: "Londres",
+    options: [
+      "Londres",
+      "Berlim",
+      "Madri",
+      "Viena"
+    ]
+  },
+    {
+    numb: 2,
+    question: "O objetivo da equipe que foi criada pelo governo Britânico que tinha como integrante Alan Turing era quebrar o enigma, ou seja, o famoso código que os alemães usavam para enviar mensagens aos submarinos.",
+    answer: "Verdadeiro",
+    options: [
+      "Verdadeiro",
+      "",
+      "Falso",
+      ""
+    ]
+  },
+    {
+    numb: 3,
+    question: "Qual era a profissão de Alan Turing?",
+    answer: "Matemático",
+    options: [
+      "Cientista da computação",
+      "Físico",
+      "Matemático",
+      "Químico"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Qual era a guerra mencionada no filme? ",
+    answer: "2ª Guerra Mundial",
+    options: [
+      "1ª Guerra Mundial",
+      "Guerra Fria",
+      "2ª Guerra Mundial",
+      "Guerra do Golfo"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Qual o nome da máquina Alemã de criptografia?",
+    answer: "Enigma",
+    options: [
+      "Enigma",
+      "Super computador",
+      "Mistério",
+      "Criptógrafia"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
